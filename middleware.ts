18b28a23342c6221da7f3a3170ca0398bbef7e95
gabrielbadaro825 <gabrielbadaro825@gmail.com>
2026-04-10 11:28:36 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,9 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Pula arquivos internos e estáticos
+    // Ignora arquivos internos e estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Sempre executa para APIs
+    // Sempre roda para APIs
     '/(api|trpc)(.*)',
   ],
 };
