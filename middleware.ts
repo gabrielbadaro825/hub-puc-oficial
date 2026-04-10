46702e6460702abd72c114c33b7bd405510a796d
gabrielbadaro825 <gabrielbadaro825@gmail.com>
2026-04-10 11:52:19 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,19 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-// 1. Definimos que as páginas de login e cadastro são PÚBLICAS.
-// Sem isso, o segurança barra quem tenta logar, criando o erro 500.
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
 
 export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    // 2. Protege todas as outras rotas
     await auth.protect();
   }
+}, {
+  // 👇 COLOQUE A MESMA CHAVE PUBLICA AQUI
+  publishableKey: "pk_test_Y2hhcm1pbmctaWd1YW5hLTE3LmNsZXJrLmFjY291bnRzLmRldiQ"
 });
 
 export const config = {
   matcher: [
-    // Pula arquivos internos e estáticos (css, imagens, etc.)
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Sempre executa para APIs
     '/(api|trpc)(.*)',
   ],
 };
