2c26ed214638723a277fcc69b9c6a83cdebd4326
gabrielbadaro825 <gabrielbadaro825@gmail.com>
2026-04-10 11:40:33 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import { ClerkProvider } from '@clerk/nextjs'
+import './globals.css' // Se você tiver um css
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
       <html lang="pt-br">
-        <body>
-          {children}
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   )
