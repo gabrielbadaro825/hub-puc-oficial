c4b84963286bdab6c1bc3154399b79f88e89f47b
gabrielbadaro825 <gabrielbadaro825@gmail.com>
2026-04-10 11:10:18 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // Pula arquivos internos do Next.js e estáticos
+    // Pula arquivos internos e estáticos
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Sempre executa para APIs
     '/(api|trpc)(.*)',
